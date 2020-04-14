4cc99dc782c0683ee8da520386443b68195a4e2b
ashraktmohamed <ashraktmohamed486@gmail.com>
2020-04-14 23:22:18 +0200

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer footer-black  footer-white ">
+      <div class="container">
+        <div class="row">
+          <nav class="footer-nav">
+            <ul>
+              <li>
+                <a href="#">
+               contact-us
+                </a>
+              </li>
+         <li>
+                 <a href="#">
+            about us
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="credits ml-auto">
+            <span class="copyright">
+              ©
+              <script>
+                document.write(new Date().getFullYear())
+              </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
