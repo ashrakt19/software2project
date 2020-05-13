07f64bd304c7ae9111ce01f83155b9f8dbcaae2e
ashraktmohamed <ashraktmohamed486@gmail.com>
2020-05-13 22:58:58 +0200

    updateComment

--- resources/views/front-end/video/comments.blade.php
+++ resources/views/front-end/video/comments.blade.php
@@ -16,13 +16,31 @@
                         </div>
                         <div class="col-md-4 text-right">
                           <span>
-                               <i class="nc-icon nc-calendar-60"></i> : {{$comment->created_at }}
+                               <i class="nc-icon nc-calendar-60"></i> : {{$comment->created_at->diffForHumans() }}
                           </span>
                           </div>
                    </div>
                   <strong> <p style="color:firebrick ;font-size:20px">{{ $comment->comment }} </p></strong>
 
-
+                  @if(auth()->user())
+                          @if((auth()->user()->group =='admin')||auth()->user()->id==$comment->user->id)
+                          <a href="" onclick="$(this).next('div').slideToggle(1000);return false;" style=" font-size:18px;">#Edit</a>
+                          <div style="display: none">
+                                <form action="{{route('front.commentUpdate',['id'=>$comment->id])}}" method="POST">
+                                     @csrf
+                                      <textarea  class="form-control" name="comment" rows="4" >
+                                          {{$comment->comment}}
+                                      </textarea>
+                                      <button type="submit" class="btn btn-default">Edit</button>
+                                </form>
+                            </div>
+                            @endif
+                            @endif
+                       
+                        
+                          @if(!$loop->last)
+                            <hr>
+                          @endif
                   
                   @if(auth()->user())
                        @if((auth()->user()->group =='admin')||auth()->user()->id==$comment->user->id)
